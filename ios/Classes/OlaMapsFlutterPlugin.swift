0711668375a89f889c42c8c11735b9706cfea00b
sivasekarvgts <sivasekar@vishgyana.com>
2025-11-10 13:37:17 +0530

    Fix(iOS): Pass registrar to OlaMapFactory in plugin registration

--- ios/Classes/OlaMapsFlutterPlugin.swift
+++ ios/Classes/OlaMapsFlutterPlugin.swift
@@ -4,7 +4,7 @@ import OlaMapCore
 
 public class OlaMapsFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let factory = OlaMapFactory(messenger: registrar.messenger())
+    let factory = OlaMapFactory(messenger: registrar.messenger(), registrar: registrar)
     registrar.register(factory, withId: "ola_maps_flutter/map_view")
   }
 }
